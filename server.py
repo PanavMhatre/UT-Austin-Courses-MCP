6726d9f6e6365405b5feef9560bdf646a26c3882
PanavMhatre <mhatrepanav@gmail.com>
2026-03-08 15:13:25 -0500

    working mcp server connection

--- server.py
+++ server.py
@@ -4,8 +4,13 @@ from typing import Any
 from fastmcp import FastMCP
 from course_store import store
 
-
-
+mcp = FastMCP("My MCP Server")
+@mcp.tool
+def search_courses(course_id: str) -> Any:
+    course_info = store.search_courses(course_id)
+    if course_info is None:
+        return {"error": "Course not found"}
+    return course_info
 
 
 if __name__ == "__main__":
